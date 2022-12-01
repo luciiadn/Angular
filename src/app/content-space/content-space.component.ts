03f6e1ed8e5f58b2583db2c8c7798f2d3bf5f91e
Lucia Di Nardo <luciiadn@gmail.com>
2022-12-01 17:29:05 -0300

    DesafioDos

--- src/app/content-space/content-space.component.ts
+++ src/app/content-space/content-space.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { students } from './models/students';
 
 @Component({
   selector: 'app-content-space',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class ContentSpaceComponent {
 
+  students: students[] = [
+    new students ('Axl' 'Rose' new Date ('61'), true),
+    new students ('Izzy' 'Stradlin' new Date ('60'), false),
+    new students ('Norma' 'Mortenson' new Date ('36'), false),
+    new students ('Emma' 'Watson' new Date ('32'), true),
+  ] 
+
+  constructor () {}
+  
+  ngOnInit(): void {
+
+  }
 }
